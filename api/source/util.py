19069578f98214d475f0f2ec50281dc65ca72c43
정수현 <39879672+asdfry@users.noreply.github.com>
2022-12-07 11:08:43 +0900

    Update util.py

--- api/source/util.py
+++ api/source/util.py
@@ -1,5 +1,7 @@
+import os
 import re
 import traceback
+from pathlib import Path
 from typing import Dict, List
 
 from logger_main import logger
@@ -22,3 +24,20 @@ def create_content(query: Dict, status_code: int, message: str, data: List[Dict]
         logger.error(f"{message}, Query: {query}, Exception: {traceback_content}")
 
     return content
+
+
+def check_envs():
+    env_var_num = {"DEVICE_NUM": os.getenv("DEVICE_NUM"), "SIM_THRESHOLD": os.getenv("SIM_THRESHOLD")}
+    env_var_path = {"JAR_PATH": os.getenv("JAR_PATH"), "MODEL_PATH": os.getenv("MODEL_PATH")}
+
+    for key, value in env_var_num.items():
+        if not value:
+            logger.error(f"Invalid environment variable ({key}): {value}")
+            return False
+
+    for key, value in env_var_path.items():
+        if not value or not Path(value).exists():
+            logger.error(f"Invalid environment variable ({key}): {value}")
+            return False
+
+    return True
