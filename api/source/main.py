c4d87fc852ba196f3c3e9afeba673e70af7e6757
정수현 <39879672+asdfry@users.noreply.github.com>
2022-12-16 11:03:37 +0900

    Update main.py

--- api/source/main.py
+++ api/source/main.py
@@ -1,13 +1,11 @@
 import sys
+from util import check_envs
+
+if not check_envs():
+    sys.exit(1)
 
 from fastapi import FastAPI
 from router import router
-from util import check_envs
 
 app = FastAPI()
 app.include_router(router)
-
-@app.on_event("startup")
-def startup_event():
-    if not check_envs():
-        sys.exit(1)
